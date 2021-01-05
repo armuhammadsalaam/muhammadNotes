a669d2562e0903d392eb82bb9e35043e5108991b
armuhammadsalaam <armuhammadsalaam@gmail.com>
2021-01-05 12:34:21 -0600

    making a locally hosted server

--- nodeProjects/sampleServer.js
+++ nodeProjects/sampleServer.js
@@ -1,15 +1,29 @@
 const http = require("http");
+const { type } = require("os");
+const fs = require("fs");
 
+// routing requests
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter text here:</title></head>");
     res.write(
       '<body><form action="/message" method="POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>'
     );
+    // name for the form does not have to be 'message'; the server will add any input data to the request and make it accessible via the assigned name.
     res.write("</html>");
-    res.end();
+    return res.end();
+    // the above return res.end() functionality ends the execution of the code function it is contained in; it will not run the rest of the code in the function.
+  }
+  //redirecting requests
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "sampleText");
+    //res.writeHead((statusCode: 302), {});
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
@@ -20,3 +34,8 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(9999);
+
+
+// refer to documentation for information about streams and buffers for more detail about parsing/getting data from request bodies.
+// data over the network is tranferred through a data 'stream' in several request body chunks until it is fully parsed 
+// 
--- nodeProjects/serverApp.js
+++ nodeProjects/serverApp.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 // creates server and executes function for every incoming request
 
@@ -10,20 +10,20 @@ http.createServer(function(req, res){
 */
 
 // use an arrow function for shorter syntax when creating a server to respond to incoming requests
-// output the request object to the console to see what's inside 
+// output the request object to the console to see what's inside
 // create a const server and set it equal to the server functionality
-// you can use process.exit(); syntax to force quit the current process for the executed code 
+// you can use process.exit(); syntax to force quit the current process for the executed code
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>serverAppTest</title></head>");
-    res.write("<body><h1>This is a test server from Node.js Server</h1></body>");
-    res.write("</html>");
-    res.end();
-    // any code passed in after the res.end() method will not be executed, since the server response will have ended.
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>serverAppTest</title></head>");
+  res.write("<body><h1>This is a test server from Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
+  // any code passed in after the res.end() method will not be executed, since the server response will have ended.
 });
 
 // use server.methods for functionality
