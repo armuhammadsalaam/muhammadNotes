dddbe11730b85af86678877a34332fd5154a837e
Muhammad <armuhammadsalaam@gmail.com>
2021-01-05 19:07:43 -0600

    listen for events on the server

--- nodeProjects/sampleServer.js
+++ nodeProjects/sampleServer.js
@@ -19,6 +19,12 @@ const server = http.createServer((req, res) => {
   }
   //redirecting requests
   if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end'); // to be executed once the server is done parsing incoming requests
     fs.writeFileSync("message.txt", "sampleText");
     //res.writeHead((statusCode: 302), {});
     res.statusCode = 302;
@@ -38,4 +44,7 @@ server.listen(9999);
 
 // refer to documentation for information about streams and buffers for more detail about parsing/getting data from request bodies.
 // data over the network is tranferred through a data 'stream' in several request body chunks until it is fully parsed 
-// 
+// use a buffer to organize incoming chunks of data; allows server program to hold and work with several pieces of information before they are sent back to the client from the server.
+// use req.on(); method to listen to incoming events that pass through the server.
+// pass in the type of event into req.on('type of event'); that you need to listen for.
+// use the req.on(); method to create a function that executes every time the particular event passes through the listener.
